8c6221cd1478a4118faec1a99c68a1d1ac0f7527
sshmatrix <cryptosauron@protonmail.com>
2023-05-02 08:43:56 +0530

    better

--- scheduler.sh
+++ scheduler.sh
@@ -3,33 +3,33 @@
 # Get database config
 source ./.env
 
-# Function to run ipns pin add command
-function pin_ipns {
-  ipns=$1
-  ipns pin add "$ipns"
+# Function to run IPNS pin add command
+function PIN_IPNS {
+  IPNS=$1
+  ipfs pin add "$IPNS"
 }
 
 # Main loop
 while true
 do
   # Get current timestamp and 24 hours ago timestamp
-  current_timestamp=$(date +%s)
-  twenty_four_hours_ago=$(($current_timestamp - 86400))
+  CURRENT_TIMESTAMP=$(date +%s)
+  ONE_DAY_AGO=$(($CURRENT_TIMESTAMP - 86400))
 
   # Get IPNS values and timestamps between 24 hours - 10 minutes and 24 hours + 10 minutes
-  ipns_timestamps=$(mysql -u $(MYSQL_USER) -p$(MYSQL_PASSWORD) -D $(MYSQL_DATABASE) -e "SELECT IPNS, timestamp FROM events WHERE timestamp BETWEEN $((twenty_four_hours_ago - 600)) AND $((twenty_four_hours_ago + 600))")
+  IPNS_TIMESTAMPS=$(mysql -u $(MYSQL_USER) -p$(MYSQL_PASSWORD) -D $(MYSQL_DATABASE) -e "SELECT IPNS, timestamp FROM events WHERE timestamp BETWEEN $((ONE_DAY_AGO - 600)) AND $((ONE_DAY_AGO + 600))")
 
   # Loop through each IPNS and pin it
-  while read -r ipns_timestamp
+  while read -r IPNS_TIMESTAMP
   do
-    ipns=$(echo "$ipns_timestamp" | awk '{print $1}')
-    timestamp=$(echo "$ipns_timestamp" | awk '{print $2}')
-    if [ -n "$ipns" ]
+    IPNS=$(echo "$IPNS_TIMESTAMP" | awk '{print $1}')
+    TIMESTAMP=$(echo "$IPNS_TIMESTAMP" | awk '{print $2}')
+    if [ -n "$IPNS" ]
     then
-      # Run pin_ipns function in background thread
-      pin_ipns "$ipns" &
+      # Run PIN_IPNS function in background thread
+      PIN_IPNS "$IPNS" &
     fi
-  done <<< "$ipns_timestamps"
+  done <<< "$IPNS_TIMESTAMPS"
 
   # Wait 10 minutes before repeating the loop
   sleep 600
